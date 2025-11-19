c03be64b83fde2361a86e557c1f2ea1428261a4b
dpb-bah <boos_daniel@bah.com>
2025-11-19 15:23:21 -0500

    Adding ExternalDataLoadDateDelta, splitting DEFCDelta to two models

--- brus_backend_common/models/__init__.py
+++ brus_backend_common/models/__init__.py
@@ -1,10 +1,12 @@
 # from brus_backend_common.models.broker_submissions import *
 # from brus_backend_common.models.broker_external import *
-from brus_backend_common.models.reference import DEFCDelta
+from brus_backend_common.models.reference import DEFCDeltaInt, DEFCDeltaRaw, ExternalDataLoadDateDelta
 
 # from brus_backend_common.models.usas import *
 
 DELTA_MODEL_CLASSES = [
-    DEFCDelta,
+    DEFCDeltaRaw,
+    DEFCDeltaInt,
+    ExternalDataLoadDateDelta,
 ]
 DELTA_MODELS = {model.table_name: model for model in DELTA_MODEL_CLASSES}
--- brus_backend_common/models/reference.py
+++ brus_backend_common/models/reference.py
@@ -12,9 +12,28 @@ from pyspark.sql.types import (
 from brus_backend_common.models.delta_model import DeltaModel
 
 
-class DEFCDelta(DeltaModel):
-    s3_bucket = "dti-broker-emr-qat"  # TODO: broker-external, broker-submissions, usas, analytics, etc.
+class DEFCDeltaRaw(DeltaModel):
+    s3_bucket = "dti-delta-reference-nonprod"  # TODO: edit for prod/nonprod
     database = "raw"
+    table_name = "defc"
+    format = "csv"
+    pk = "DEFC_CODE"
+    unique_constraints = []
+    migration_history = []
+
+    @property
+    def structure(self):
+        return StructType(
+            [
+                StructField("DEFC_CODE", StringType(), False),
+                StructField("DEFC_TITLE", StringType(), False),
+            ]
+        )
+
+
+class DEFCDeltaInt(DeltaModel):
+    s3_bucket = "dti-delta-reference-nonprod"  # TODO: edit for prod/nonprod
+    database = "int"
     table_name = "defc"
     pk = "defc_id"
     unique_constraints = ["code"]
@@ -34,5 +53,29 @@ class DEFCDelta(DeltaModel):
                 StructField("urls", ArrayType(StringType(), True), True),
                 StructField("is_valid", BooleanType(), False),
                 StructField("earliest_pl_action_date", TimestampType(), True),
+            ]
+        )
+
+
+class ExternalDataLoadDateDelta(DeltaModel):
+    s3_bucket = "dti-delta-reference-nonprod"  # TODO: edit for prod/nonprod
+    database = "int"
+    table_name = "external_data_load_date"
+    format = "csv"
+    pk = "external_data_load_date_id"
+    unique_constraints = ["name"]
+    migration_history = []
+
+    @property
+    def structure(self):
+        return StructType(
+            [
+                StructField("created_at", TimestampType(), True),
+                StructField("updated_at", TimestampType(), True),
+                StructField("external_data_load_date_id", IntegerType(), False),
+                StructField("name", StringType(), False),
+                StructField("description", StringType(), False),
+                StructField("last_load_date_start", TimestampType(), False),
+                StructField("last_load_date_end", TimestampType(), False),
             ]
         )
