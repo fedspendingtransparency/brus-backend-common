9b87697a7470c195dbc93d160b80f992fbe32fed
Daniel Boos <boos_daniel@bah.com>
2026-05-06 15:41:24 -0400

    getattr <> @property

--- brus_backend_common/helpers/aws.py
+++ brus_backend_common/helpers/aws.py
@@ -138,8 +138,14 @@ def _get_boto3(method_name: str, *args, region_name=CONFIG.AWS_REGION, **kwargs)
     attr = getattr(boto3, method_name)
     kwargs.update({"region_name": region_name})
     endpoint = None
-    if len(args) > 0 and args[0].upper() in ("S3", "SSM", "STS"):
-        endpoint = getattr(CONFIG, f"AWS_{args[0].upper()}_ENDPOINT")
+    # Note: we can't dynamically pull these as getattr <> @property CONFIG.AWS_S3_ENDPOINT
+    endpoint_mappings = {
+        'S3': CONFIG.AWS_S3_ENDPOINT,
+        'SSM': CONFIG.AWS_SSM_ENDPOINT,
+        'STS': CONFIG.AWS_STS_ENDPOINT
+    }
+    if len(args) > 0 and args[0].upper() in endpoint_mappings:
+        endpoint = endpoint_mappings[args[0].upper()]
 
     if callable(attr):
         if CONFIG.IS_LOCAL:
