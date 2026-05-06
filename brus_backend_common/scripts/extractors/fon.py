6e5eb6bd8c4c1e483d80c171fa287b70582e7cd8
Daniel Boos <boos_daniel@bah.com>
2026-05-06 15:28:18 -0400

    Including region

--- brus_backend_common/scripts/extractors/fon.py
+++ brus_backend_common/scripts/extractors/fon.py
@@ -57,7 +57,7 @@ def main(metrics: dict = None):
     s3 = _get_boto3("client", "s3")
     logger.info(f"S3 CLIENT REGION: {s3.meta.region_name}")
     # s3.upload_file(local_fon_csv, fon_bronze.BUCKET_NAME, fon_bronze.RELATIVE_CSV_PATH)
-    s3_local = f'aws s3 cp {local_fon_csv} s3://{fon_bronze.BUCKET_NAME}/{fon_bronze.RELATIVE_CSV_PATH}'
+    s3_local = f'aws s3 cp {local_fon_csv} s3://{fon_bronze.BUCKET_NAME}/{fon_bronze.RELATIVE_CSV_PATH} --region {CONFIG.AWS_REGION}'
     logger.info(s3_local)
     os.system(s3_local)
 
