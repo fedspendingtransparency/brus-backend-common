79e6c1ac1363198570a0dce53e1a12d8d8076dec
Daniel Boos <boos_daniel@bah.com>
2026-05-06 14:45:35 -0400

    [DEV-12155] Adding s3 client region to temporary logs

--- brus_backend_common/scripts/extractors/fon.py
+++ brus_backend_common/scripts/extractors/fon.py
@@ -53,6 +53,7 @@ def main(metrics: dict = None):
     logger.info(f"RELATIVE_CSV_PATH: {fon_bronze.RELATIVE_CSV_PATH}")
     logger.info(f"AWS_REGION: {CONFIG.AWS_REGION}")
     s3 = _get_boto3("client", "s3")
+    logger.info(f"S3 CLIENT REGION: {s3.meta.region_name}")
     s3.upload_file(local_fon_csv, fon_bronze.BUCKET_NAME, fon_bronze.RELATIVE_CSV_PATH)
 
     metrics["end_time"] = get_utc_now()
