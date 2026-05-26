36ef9f2c17f6dd8c736aadd741d9d523d23c585b
Daniel Boos <boos_daniel@bah.com>
2026-05-26 16:05:56 -0400

    [DEV-12157] Adding ProgramActivityGold (no loader required)

--- brus_backend_common/models/__init__.py
+++ brus_backend_common/models/__init__.py
@@ -1,29 +1,31 @@
 from brus_backend_common.models.lakehouse_model import LakeHouseCurrentMigration, ExternalDataLoadDate
 
 from brus_backend_common.models.reference import (
+    AgencyBronze,
+    CGACGold,
     DEFCBronze,
     DEFCGold,
     DEFCGroup,
     FONBronze,
     FONGold,
-    AgencyBronze,
-    CGACGold,
     FRECGold,
+    ProgramActivityGold,
     SubTierAgencyGold,
 )
 
 LAKEHOUSE_MODEL_CLASSES = [
     AgencyBronze,
     CGACGold,
-    FRECGold,
-    SubTierAgencyGold,
     DEFCBronze,
     DEFCGroup,
     DEFCGold,
+    ExternalDataLoadDate,
     FONBronze,
     FONGold,
+    FRECGold,
     LakeHouseCurrentMigration,
-    ExternalDataLoadDate,
+    ProgramActivityGold,
+    SubTierAgencyGold,
 ]
 LAKEHOUSE_MODELS = {}
 for model in LAKEHOUSE_MODEL_CLASSES:
